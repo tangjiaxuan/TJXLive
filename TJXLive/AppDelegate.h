6838b3e324a4d5bf810b4da1bd75c14e0ab15a9d
tangjiaxuanmac <tangjiaxuanmac@tangjiaxuanmacdemac.local>
2017-07-14 23:29:46 -0700

    Initial Commit

---
+++ TJXLive/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TJXLive
+//
+//  Created by tangjiaxuanmac on 2017/7/14.
+//  Copyright © 2017年 tangjiaxuanmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TJXLiveTests/TJXLiveTests.m
@@ -0,0 +1,39 @@
+//
+//  TJXLiveTests.m
+//  TJXLiveTests
+//
+//  Created by tangjiaxuanmac on 2017/7/14.
+//  Copyright © 2017年 tangjiaxuanmac. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TJXLiveTests : XCTestCase
+
+@end
+
+@implementation TJXLiveTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
